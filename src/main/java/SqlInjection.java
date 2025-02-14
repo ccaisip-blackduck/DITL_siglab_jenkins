ec4b4c4fb9c8bf27a12d0071aad337d6ba6def5a
ccaisip-blackduck <ccaisip@blackduck.com>
2025-02-14 10:06:45 -0800

    Update SqlInjection.java

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -18,4 +18,12 @@ public class SqlInjection {
         ResultSet results = statement.executeQuery(query);
         return(results);
     }
+
+    public static ResultSet doQuery_demo_cc_0214(HttpServletRequest request, Connection connection) throws SQLException {
+        String customerName_cc0214 = request.getParameter("customerName");
+        String query = "SELECT account_balance FROM user_data WHERE user_name = " + customerName_cc0214;
+        Statement statement = connection.createStatement();
+        ResultSet results = statement.executeQuery(query);
+        return(results);
+    }
 }
